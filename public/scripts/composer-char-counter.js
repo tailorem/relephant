9353c833af9a9884c0be1a0980aeaaf087015011
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-17 20:31:15 +0000

    implemented character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,17 @@
+//jshint esversion: 6
+
 $(document).ready(function() {
-  $('section.new-tweet textarea').on('keyup', function(event) {
-    let current = $(this).val().length;
-    return 140 - current;
+  // 'input' eliminates counter lag from 'keyup'
+  $('section.new-tweet textarea').on('input', function(event) {
+    let count = 140 - this.value.length;
+    // $variable distinguishes jQuery objects from other variables
+    let $counter = $(this).siblings('.counter');
+    if (count >= 0) {
+      $counter.text(count).removeClass('neg-count');
+    } else {
+      $counter.text(count).addClass('neg-count');
+    }
+    // $counter.text(count);
+    // console.log($counter);
   });
 });
