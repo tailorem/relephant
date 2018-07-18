e6cc6870bd024ae870c727107c51f6301d0a89db
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-18 06:43:16 +0000

    implemented renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,22 @@
  */
 
 // Temporarily hard-coded tweet object for testing
-var tweetData = {
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Taylour",
+      "avatars": {
+        "small":   "/images/icon.jpg"
+      },
+      "handle": "@tailorem"
+    },
+    "content": {
+      "text": "Something thoughtful should go here, but it won't."
+    },
+    "created_at": "X moment in time"
+  },
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -19,31 +34,66 @@ var tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
-  };
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
+function renderTweets(tweets) {
+  // loops through tweets
+  for (var index in data) {
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    var $tweet = createTweetElement(data[index]);
+    // console.log(tweet);
+    $('#tweets').append($tweet);
+  }
 
-function createTweetElement(tweetData) {
-  // var $newTweet = tweetData;
-  var $article = $("<article>").addClass("tweet");
-  var icons ='<i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>';
-
-  $article.append('<header><img class="avatar" src="' + tweetData.user.avatars.small + '" /><h3>' + tweetData.user.name + '</h3><span>' + tweetData.user.handle)
-  .append("<p>" + tweetData.content.text + "</p>")
-  .append('<footer><p>' + tweetData.created_at + '</p><p class="icons">' + icons + '</p>');
-
-  // .append("<h3>" + tweetData.user.name + "</h3>");
-
-  // $('article').append('<header>' + tweetData.user.name + '</header>');
-  // console.log($article[0]);
-  return $article[0];
+  // console.log(tweets);
+  // $('#tweets').append($tweet);
 }
 
-var $tweet = createTweetElement(tweetData);
 
-// // Test / driver code (temporary)
+function createTweetElement(tweet) {
+  var $tweet = $("<article>").addClass("tweet");
+  var icons ='<i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>';
+
+  $tweet.append('<header><img class="avatar" src="' + tweet.user.avatars.small + '" /><h3>' + tweet.user.name + '</h3><span>' + tweet.user.handle)
+  .append("<p>" + tweet.content.text + "</p>")
+  .append('<footer><p>' + tweet.created_at + '</p><p class="icons">' + icons + '</p>');
+
+  return $tweet;
+}
+
 
 $( document ).ready(function() {
-  // console.log($tweet);
-  $('#tweets').append($tweet);
+  renderTweets(data);
 });
 
