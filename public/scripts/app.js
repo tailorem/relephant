3e3b441388589fb8c09a2919b62855c8ee404a20
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-19 00:43:21 +0000

    implemented instant page refresh on submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,45 +6,18 @@
 
 $(function() {
 
-  // Temporarily hard-coded tweet object for testing
-  // Fake data taken from tweets.json
-  // var data = [
-  //   {
-  //     "user": {
-  //       "name": "Taylour",
-  //       "avatars": {
-  //         "small":   "/images/icon.jpg"
-  //       },
-  //       "handle": "@tailorem"
-  //     },
-  //     "content": {
-  //       "text": "Something thoughtful should go here, but it won't."
-  //     },
-  //     "created_at": 1531928614954
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   }
-  // ];
-
   function renderTweets(tweetData) {
+    $('#tweets').empty();
+    rTweets = tweetData.map(createTweetElement).reverse();
     // May need to remove .reverse()
-    $('#tweets').append(tweetData.map(createTweetElement).reverse());
+    // console.log(tweetData);
+    $('#tweets').append(rTweets);
   }
 
   function createTweetElement(tweet) {
+
+    // console.log(tweet.user);
+
     var $tweet = $("<article>").addClass("tweet");
 
     var $header = $("<header>").appendTo($tweet);
@@ -61,29 +34,48 @@ $(function() {
     return $tweet;
   }
 
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets"
+    })
+    .done(function(tweets) {
+      // console.log(tweets);
+      renderTweets(tweets);
+    });
+  }
+
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
+
+    var input = event.target.elements.text.value;
+
+    if (input.length > 140) {
+      alert("Sorry, your tweet cannot exceed 140 characters.");
+      return;
+    }
+    if (input.trim().length < 1) {
+      // event.preventDefault();
+      alert("Oops, you can't post a blank tweet.");
+      return;
+    }
 
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: $(this).serialize()
-    });
-
-    console.log(event.target.elements.text.value);
-    event.target.elements.text.value = "";
-    console.log($(this).serialize());
-  });
-
-  function loadTweets() {
-    $.ajax({
-      method: "GET",
-      url: "/tweets",
     })
-    .done(function(tweets) {
-      renderTweets(tweets);
+    .done(function() {
+
+      loadTweets();
+
     });
-  }
+
+    // RESET COUNTER
+    // console.log($(this).children()[2]);
+    input = "";
+    // console.log($(this).serialize());
+  });
 
   loadTweets();
 
